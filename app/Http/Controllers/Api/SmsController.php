f865d96ccc13d0af6b1a46d4a002a8ff0546e7ac
Binaya-Ghimire <binayaghimire1974@gmail.com>
2021-03-05 17:38:33 +0545

    send msg from api call using user auth token

--- app/Http/Controllers/Api/SmsController.php
+++ app/Http/Controllers/Api/SmsController.php
@@ -3,53 +3,83 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\UserToken;
+use App\Models\SmsHistory;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\Admin\SendSms;
 
 class SmsController extends Controller
 {
     public function sendSms(Request $request)
     {
         $userToken = UserToken::where('token', $request->token)->first();
-    	if ($userToken->status == 0) {
-    		echo "The key is not Active at the moment";
-    	}else{
-    		$user = $userToken->user;
-    		if(!is_null($user->banned_at))
-    		{
-    			echo "Your Account was Banned from using this service";
-    		}else{
-                $message = "hello this is test"; //char =200 (2)
-                $phone_number = "9841984123,9849654318";
-    			$balances = $user->UserBalance;
-    			$total_balance= $user->UserBalance->sum('balance');
-    			$total_coins= $user->UserBalance->sum('coins');
-                //need to write function to calculate required coins
-                $required_coins=10;
-    			if($total_coins < $required_coins){
-    				echo("You do not have sufficient balance to send the sms");
-    			}else{
-    				
-                    //need a function to save this message history 
-                    //
-    				$this->send($message, $phone_number);
-                    //success - update coin (totaloin - required coin)
-    			}
-    		}
-    	}   	
+        if ($userToken->status == 0) {
+            echo "The key is not Active at the moment";
+        }else{
+            $user = $userToken->user;
+            if(!is_null($user->banned_at))
+            {
+                echo "Your Account was Banned from using this service";
+            }else{
+                $message = $request->message;
+                $phone_number = $request->number;
+                $required_coins = $this->calculateCoin($message);       
+                $total_coins= $user->UserBalance->sum('coins');
+
+                if($total_coins < $required_coins){
+                    echo("You do not have sufficient balance to send the sms");
+                }else{
+                    // $this->send($message, $phone_number);
+                    SmsHistory::create([
+                        'send_by'=>$user->id,
+                        'send_to'=>$phone_number,
+                        'message'=>$message,
+                        'coins_used'=>$required_coins,
+                    ]);
+                    $remaining_balance = $this->deductCoin($user, $required_coins);
+                    $msg = "msg sent to ".$phone_number."with coin cost ".$required_coins." and remaining balance is ". $remaining_balance;
+                    return $msg;
+                }
+            }
+        }       
     }
+
+
+   private function calculateCoin($message)
+    {
+        $msg_length = Str::length($message);
+        $required_coins = intdiv($msg_length, 160);
+        $required_coins= $required_coins+1;
+        return $required_coins;
+    }
+
     public function send($message, $phone_number)
     {
-    	echo "ready to send sms with";
-        echo "<br>"."message:". $message;
-        echo "<br>"."to :".$phone_number;
+        $send = new SendSms();
+         $msg = $send->sendsms($phone_number, $message);
+        echo $msg;
+
     }
 
-
-    public function viewBalance($token)
+    public function deductCoin($user, $required_coins)
     {
-        $userToken = UserToken::where('token', $token)->first();
-        $balances = $userToken->user->UserBalance;
-        return $balances;
+        foreach ($user->Userbalance as $balance) {
+             if($balance->coins > $required_coins){
+                $balance->decrement('coins', $required_coins);
+                break;
+             }elseif($balance->coins == $required_coins){
+                $balance->decrement('coins', $required_coins);
+                $balance->delete();
+             }else{
+                $extra = $required_coins - $balance->coins;
+                $balance->coins = 0;
+                $balance->delete();
+                $required_coins = $extra;
+             }
+         }
+        $remaining_balance = $user->Userbalance->sum('coins');
+        return $remaining_balance; 
     }
+
 }
